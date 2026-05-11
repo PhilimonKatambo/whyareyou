3b8ba485dcfc48d5f6bfb78cf0f97a71ccaf9b2f
philimon <phidophidz@gmail.com>
2026-05-11 09:17:48 +0200

    why are you

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [answer, setAnswer] = useState("");
+  const [show, setShow] = useState(false);
+  const [analyse, setAnalyse] = useState("");
+
+
+  const showw = () => {
+    setAnalyse("Analysing your answer");
+
+    let dots = 0;
+
+    const interval = setInterval(() => {
+      dots++;
+
+      setAnalyse("Analysing your answer" + ".".repeat(dots));
+
+      if (dots >= 10) {
+        clearInterval(interval);
+
+        setTimeout(() => {
+          setShow(true);
+        }, 500);
+      }
+    }, 300);
+  };
+
+  return (
+    <div id="bd">
+      <img src='assets/download.jpg'></img>
+      <div id="big">
+        <div id="question">Why are you gay?</div>
+
+        <input
+          type="text"
+          id="answer"
+          placeholder="Answer here"
+          onChange={(e) => {
+            setAnswer(e.target.value);
+            setAnalyse("");
+            setShow(false)
+          }}
+        />
+
+        <button
+          id="verify"
+          onClick={() => {
+            if (answer.trim() !== "") {
+              showw();
+            }
+          }}
+        >
+          Verify
+        </button>
+      </div>
+
+      <div id="show">
+        <div id="ans">{analyse}</div>
+
+        {show && (
+          <div id="real">
+            The truth is that you're <strong>Gay</strong>.
+            <br></br>
+            <div style={{marginTop: "20px", color:"#929293"}}>And you have nothing to do about it.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
